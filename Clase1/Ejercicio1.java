6a2dfa2d76f7bf87ee7ce874ae0bcd44612885f5
LinkDevArch <jstivenbedoya@estudiante.uniajc.edu.co>
2025-02-04 17:20:49 +0000

    Suma extremos arreglos

--- Clase1/Ejercicio1.java
+++ Clase1/Ejercicio1.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -5,12 +6,13 @@ public class Ejercicio1 {
     public static void main (String[] args) {
         
         sumaDeNumeros();
+        sumaDeExtremos();
 
     }
 
     public static void sumaDeNumeros () {
+        
         ArrayList<Integer> numbers = new ArrayList<>();
-
         Collections.addAll(numbers, 2,5,8,3,1,10,11);
         
 
@@ -26,5 +28,20 @@ public class Ejercicio1 {
 
         System.out.println("suma de pares "+sumaNumerosPares);
         System.out.println("suma de impares "+sumaNumerosImpares);
+    }
+
+    public static void sumaDeExtremos(){
+        int []num = {4,8,2,1,6,7};
+
+
+        String suma = "";
+        
+        for(int i =0; i < num.length/2; i++){
+            suma += (num[i]+num[num.length-1-i])+" ";
+        }
+
+        System.out.println(suma);
+
+        
     }
 }
